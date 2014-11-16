2474f3726bc9dfc8a76db7c0c792d9e69ca8cded
Jordan Kasper <jordan@jordankasper.com>
2014-11-16 17:38:27 -0500

    Added main file

---
+++ srcByLine.js
@@ -0,0 +1,110 @@
+(function() {
+    'use strict';
+
+    var fileCache = {},
+        nodeList = [].slice.call(document.querySelectorAll('[data-srcbyline]'));
+
+    nodeList.forEach(processNode);
+
+    function processNode(node) {
+        var input = node.dataset.srcbyline.split(/\?/),
+            filename = input[0],
+            ext = filename && filename.split(/\./)[1],
+            lineNums = input[1] && input[1].split(/\;/);
+
+        if (!filename || !lineNums) {
+            return;
+        }
+
+        if (fileCache[filename]) {
+            // we already have it...
+            insertLines(node, fileCache[filename], lineNums);
+
+        } else if (fileCache[filename] === null) {
+            // it's being processed now, so hang out and try again shortly...
+            setTimeout(function() {
+                processNode(node);
+            }, 100);
+        } else {
+            getSource(filename, function(data) {
+                if (data !== null) {
+                    fileCache[filename] = {
+                        filename: filename,
+                        lines: data
+                    };
+                    insertLines(node, fileCache[filename], lineNums);
+                }
+            });
+        }
+    }
+
+    function insertLines(node, data, lineNums) {
+        var content = [];
+
+        lineNums.forEach(function(lineSelect) {
+            var lines;
+
+            if (/^\s*[0-9]+\s*$/.test(lineSelect)) {
+                content.push(data.lines[Number(lineSelect)] || '');
+
+            } else if (/^\s*[0-9]+\-[0-9]+\s*$/.test(lineSelect)) {
+                lines = getNumbersInRange(lineSelect);
+                lines.forEach(function(line) {
+                    content.push(data.lines[line-1] || '');
+                });
+            }
+        });
+
+        node.innerHTML = highlight(content.join("\n"));
+    }
+
+    function getNumbersInRange(range) {
+        var i,
+            nums = [],
+            ends = range.split(/\-/);
+
+        if (!ends || ends.length !== 2) {
+            return nums;
+        }
+
+        for (i=ends[0]; i<=ends[1]; ++i) {
+            nums.push(i);
+        }
+
+        return nums;
+    }
+
+    function highlight(content) {
+        var parsed = content;
+
+        if (window.hljs) {
+            try {
+                parsed = hljs.highlightAuto(content).value;
+            } catch(err) {
+                console.warn(err);
+                parsed = content;
+            }
+        }
+
+        return parsed;
+    }
+
+    function getSource(filename, cb) {
+        var srcXhr = new XMLHttpRequest();
+        cb = (cb || function(){});
+
+        srcXhr.open("GET", filename, true);
+        srcXhr.onreadystatechange = function(){
+            if (srcXhr.readyState >= 4) {
+                if (srcXhr.status == 200) {
+                    cb(srcXhr.responseText.split("\n"));
+                } else  {
+                    console.warn(srcXhr.status, srcXhr.responseText);
+                    cb(null);
+                }
+            }
+        };
+        srcXhr.send();
+    }
+
+})();
